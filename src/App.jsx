ac1f5062c525e41a20396b549ddd3e8d3636bf21
Sara Rampazzo <sararampazzo.ph@gmail.com>
2025-02-27 10:28:06 +0100

    feat: removed and fixed some things

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import GithubUser from "./GithubUser";
-import { SWRConfig } from "swr";
-
-const fetcher = (...args) =>
-  fetch(...args).then((res) => {
-    if (!res.ok) {
-      throw new Error("API request failed");
-    }
-    return res.json();
-  });
 
 function App() {
-  const handleLogin = (data) => {
-    console.log("Login Data:", data);
-  };
-
   return (
-    <SWRConfig value={{ fetcher }}>
-      <div>
-        <GithubUser username="sarablue17" />
-      </div>
-    </SWRConfig>
+    <div>
+      <GithubUser username="sarablue17" />
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { SWRConfig } from "swr";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
+createRoot(document.getElementById("root")).render(
+  <SWRConfig value={{ fetcher }}>
     <App />
-  </StrictMode>,
-)
+  </SWRConfig>
+);
